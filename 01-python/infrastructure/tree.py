f4391018638d4e50006047a76ae6114ef53fb3c5
caik20 <caik20@bu.edu>
2020-01-27 17:20:17 -0500

    tree

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,26 @@
 def draw_tree():
-    print("tree not found")
+    #print("tree not found")
+    draw_leaves()
+    draw_trunk()
+
     return
+
+
+def draw_leaves():
+    for i in range(5):
+        leaves = ""
+        for j in range(10):
+             if j < 5-i:
+                leaves += " "
+             elif j > 5+i:
+                leaves += " "
+             else:
+                leaves += "*"
+        print(leaves)
+    return
+
+def draw_trunk():
+    for i in range(3):
+        s = " " *4 + "|" + " " + "|" + " "*3
+        print(s)
+    return          
