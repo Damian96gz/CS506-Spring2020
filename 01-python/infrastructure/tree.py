6eb3a5a8509974958f3f7ef6cc092f8fd48defaa
caik20 <caik20@bu.edu>
2020-01-29 16:50:08 -0500

    tree, add variable

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,25 +1,27 @@
-def draw_tree():
-    draw_leaves()
-    draw_trunk()
+def draw_tree(height=5):
+    draw_leaves(height)
+    draw_trunk(height)
 
     return
 
 
-def draw_leaves():
-    for i in range(5):
+def draw_leaves(height):
+    for i in range(height):
         leaves = ""
-        for j in range(10):
-             if j < 5-i:
+        for j in range(2*height):
+             if j < height-i:
                 leaves += " "
-             elif j > 5+i:
+             elif j > height+i:
                 leaves += " "
              else:
                 leaves += "*"
         print(leaves)
     return
 
-def draw_trunk():
-    for i in range(3):
-        s = " " *4 + "|" + " " + "|" + " "*3
+def draw_trunk(height):
+
+    for i in range(height-3):
+        s = " " *(height-1) + "|" + " " + "|" 
         print(s)
+    
     return          
